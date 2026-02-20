9c9091173dc4e2b6ef0ad52cab96aa7bbe4774ed
Marcio Bueno <marciobueno@gmail.com>
2026-02-20 00:29:40 +0000

    Primeiro componente React

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       Olá, Turma!
+      <Profile />
+      <Profile />
     </div>
   );
 }
