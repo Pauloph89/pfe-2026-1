6db765d088cdf0be6b0092b4acc7cee1be46491a
Marcio Bueno <marciobueno@gmail.com>
2026-03-12 21:24:50 -0300

    Exemplo: projeto números aleatórios

---
+++ next-02-aleatorio/app/page.js
@@ -0,0 +1,10 @@
+import GeradorAleatorio from "@/components/GeradorAleatorio";
+//import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <GeradorAleatorio />
+    </div>
+  );
+}
